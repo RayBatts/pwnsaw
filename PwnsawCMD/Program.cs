5006fdf99a63468c0e755b78480d2b76e2508a87
Ray Batts <batts.ray@gmail.com>
2016-02-03 21:03:41 -0600

    CMD line commit test.

---
+++ PwnsawCMD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PwnsawCMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( "Pwnsaw 1.0" );
+            Console.ReadLine();
+        }
+    }
+}
